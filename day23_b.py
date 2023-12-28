05ce04d7e5e4ab000d277aa6e24e8422676d14df
Alexandra Maximova <alexandra.maximova@inf.ethz.ch>
2023-12-28 13:04:07 +0100

    Try to solve Day 19 a, Solve Day 23 b

--- day23_b.py
+++ day23_b.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 def parse_data(file_path):
     with open(file_path, 'r') as file:
         data = [line.strip() for line in file]
@@ -14,44 +16,67 @@ def get_neighbors(field, data):
     return [neighbor for neighbor in neighbors if 0 <= neighbor[0] < n and 0 <= neighbor[1] < m and data[neighbor[0]][neighbor[1]] != '#']
 
 
-def bfs(start, data):
+def get_graph_nodes(start, end, data):
+    graph_nodes = []
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            if data[i][j] != '#' and len(get_neighbors((i, j), data)) > 2:
+                graph_nodes.append((i, j))
+    graph_nodes.append(start)
+    graph_nodes.append(end)
+    return graph_nodes
+
+
+def summarize_graph(start, end, data):
+    graph = {}
     n = len(data)
     m = len(data[0])
-    visited = [[-2 if data[i][j] == '#' else -1 for j in range(m)] for i in range(n)]
-    visited[start[0]][start[1]] = 0
-    queue = [start]
+    graph_nodes = get_graph_nodes(start, end, data)
+    for graph_node in graph_nodes:
+        graph.setdefault(graph_node, find_graph_node_neighbors(graph_node, graph_nodes, data))
+    return graph
 
+
+def find_graph_node_neighbors(start, graph_nodes, data):
+    n = len(data)
+    m = len(data[0])
+    graph_neighbors = []
+    queue = [(start, 0)]
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    visited[start[0]][start[1]] = True
     while queue != []:
-        current = queue.pop(0)
-        neighbors = get_neighbors(current, data)
+        current, dist = queue.pop(0)
+        neighbors = [neighbor for neighbor in get_neighbors(current, data) if not visited[neighbor[0]][neighbor[1]]]
         for neighbor in neighbors:
-            if visited[neighbor[0]][neighbor[1]] == -1:
-                queue.append(neighbor)
-                visited[neighbor[0]][neighbor[1]] = visited[current[0]][current[1]] + 1
-        if len(neighbors) > 2:
-            print(f"There were {len(neighbors)} neighbors for {current}: {neighbors}")
+            visited[neighbor[0]][neighbor[1]] = True
+            if neighbor in graph_nodes:
+                graph_neighbors.append((neighbor, dist + 1))
+            else:
+                queue.append((neighbor, dist+1))
+    return graph_neighbors
 
-    return visited
 
-
-def longest_path(start, end, data):
+def longest_path(start, end, graph):
     steps = 0
 
-    stack = [(start, {start})]
+    stack = [(start, 0, {start})]
     while stack != []:
-        current, visited = stack.pop()
+        current, current_steps, visited = stack.pop()
         if current == end:
-            steps = max(steps, len(visited))
-        for neighbor in get_neighbors(current, data):
+            steps = max(steps, current_steps)
+        for neighbor, distance in graph[current]:
             if neighbor not in visited:
                 new_visited = visited.copy()
                 new_visited.add(neighbor)
-                stack.append((neighbor, new_visited))
+                stack.append((neighbor, current_steps + distance, new_visited))
     return steps
+
 
 file_path = 'data/day23.txt'
 data = parse_data(file_path)
-# print(longest_path((0, 1), (7, 5), data) - 1)
-print(longest_path((0, 1), (140, 139), data) - 1)
-# print(longest_path((0, 1), (22, 21), data) - 1)
-# print(bfs((0,1), data)[140][139])
+start = (0, 1)
+end = (140, 139)
+# end = (22, 21)
+graph = summarize_graph(start, end, data)
+pprint(graph)
+print(longest_path(start, end, graph))
